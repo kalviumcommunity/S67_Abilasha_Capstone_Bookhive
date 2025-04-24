b8745575d957f3f724470883ac90b6a58d98a0a9
Abilasha Manne <Abilashamanne@gmail.com>
2025-04-24 15:18:26 +0530

    done

--- backend/src/Model/Book.js
+++ backend/src/Model/Book.js
@@ -1,4 +1,3 @@
-// src/models/Book.js
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
@@ -6,7 +5,10 @@ const bookSchema = new mongoose.Schema({
   author: { type: String, required: true },
   genre: { type: String, required: true },
   description: { type: String, required: true },
-  publishedDate: { type: Date, default: Date.now }
+  publishedDate: { type: Date, default: Date.now },
+
+  // Added by: one-to-many (a user adds multiple books)
+ addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 const Book = mongoose.model('Book', bookSchema);
