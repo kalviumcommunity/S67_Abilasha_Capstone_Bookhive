bf3e0da2890aa7d8fe8466d9a9f8048a3ead3d61
Abilasha Manne <Abilashamanne@gmail.com>
2025-04-25 12:09:42 +0530

    added landing page

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // make sure this line exists!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
